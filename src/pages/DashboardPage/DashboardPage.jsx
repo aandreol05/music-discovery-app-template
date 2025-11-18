4bcb0e4f304b784c7b171984d997c0c047851f4c
ANDREOLI ANDREA <aandreol@peda.pun.univ-poitiers.fr>
2025-11-18 18:20:14 +0100

    fix: 🐛 �Remove unnecessary React import in DashboardPage component

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchUserTopArtists, fetchUserTopTracks } from '../../api/spotify-me.js';
 import { useRequireToken } from '../../hooks/useRequireToken.js';
 import SimpleCard from '../../components/SimpleCard/SimpleCard.jsx';
